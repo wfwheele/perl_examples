4e99230264d92c7001c0e16141d5de16a72d8d17
William Frank Wheeler II <wfwheele@buffalo.edu>
2016-06-23 13:39:25 -0400

    expand merge hash example

--- merge_hash.pl
+++ merge_hash.pl
@@ -5,3 +5,9 @@ use feature qw/say/;
 
 my $hash_ref = { ( foo => 'bar' ), ( bar => 'foo' ) };
 say Dumper $hash_ref;
+
+my %hash1 = ( foo => 'bar' );
+my %hash2 = ( foo => 'xap' );
+
+my %new_hash = ( %hash1, %hash2 );
+say Dumper \%new_hash;
